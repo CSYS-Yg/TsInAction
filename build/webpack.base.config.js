1fc3646d69e8bd9a4dc59f036967c8e29b3abcd2
Yx_Zou <storm.yxzou@gmail.com>
2019-07-22 19:53:40 +0800

    配置 ts 开发环境

---
+++ build/webpack.base.config.js
@@ -0,0 +1,32 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.ts',  // 配置入口文件
+    output: {
+        filename: 'app.js'    // 配置输出
+    },
+    resolve: {
+        extensions: ['.js', '.ts', '.tsx']  // 指定扩展名
+    },
+    // 选择 Ts ，需要安装相对应的 loader 。
+    // npm i ts-loader typescript -D ，这里需要注意再次本地安装 typescript
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/i,  // 尾缀正则
+                use: [{
+                    loader: 'ts-loader'
+                }],
+                exclude: /node_modules/ // 排除文件夹
+            }
+        ]
+    },
+    // 插件安装
+    plugins: [
+        // 安装 HtmlWebpackPlugin 插件，通过一个模板帮助我们生成网站首页，网页自动嵌套
+        // npm i html-webpack-plugin -D
+        new HtmlWebpackPlugin({
+            template: './src/tpl/index.html'
+        })
+    ]
+}
