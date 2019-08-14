4ebc778678ef5bd7acc07b10f7138fd19954d8d3
Yx_Zou <storm.yxzou@gmail.com>
2019-08-14 14:21:48 +0800

    26 | 配置tsconfig.json（1）：文件选项

---
+++ src/c.ts
@@ -0,0 +1,8 @@
+/*
+ * @Author: Yx_Zou 
+ * @Description: '26 | 配置 tsconfig.json（1）：文件选项' 
+ * @Date: 2019-08-14 13:30:30 
+ * @Last Modified by: Yx_Zou
+ * @Last Modified time: 2019-08-14 13:39:06
+ */
+let tsconfig: string = '123';
